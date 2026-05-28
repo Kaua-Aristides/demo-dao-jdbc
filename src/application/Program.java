2c145b615c7db89d1e4de07234140a2973d53463
Kaua-Aristides <kauaaristide@gmail.com>
2026-05-28 11:24:56 -0300

    SellerDaoJDBC and DaoFactory

--- src/application/Program.java
+++ src/application/Program.java
@@ -2,6 +2,8 @@ package application;
 
 import java.util.Date;
 
+import model.dao.DaoFactory;
+import model.dao.SellerDao;
 import model.entities.Department;
 import model.entities.Seller;
 
@@ -9,12 +11,13 @@ public class Program {
 
 	public static void main(String[] args) {
 		
-		
-		
 		Department dp = new Department(1, "Books");
 		
 		Seller sl = new Seller(1, "mario", "mario@gmail.com", new Date(), 3000.0, dp);
 		
+		SellerDao sellerDao = DaoFactory.creatSellerDao();
+		
 		System.out.println(sl);
+	
 	}
 }
